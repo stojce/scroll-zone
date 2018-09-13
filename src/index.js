f424252675eb2c00e05f1727eb0fd0e7283049a0
Stojce Slavkovski <stojce.slavkovski@alite-international.com>
2018-09-13 15:25:49 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -8,17 +8,6 @@ class ScrollZone {
    * @param {Object} options
    */
   constructor(options) {
-
-/*
-
-  scrollSize = 3;
-  zoneHeight = 30;
-  element: Element;
-
-  offsetTop = 0;
-  intervalId = null;
-
-*/
     if (options.element) {
       this.element = options.element;
     } else {
@@ -48,18 +37,13 @@ class ScrollZone {
     const topZone = evt.pageY < this.offsetTop + this.zoneHeight;
     const bottomZone = evt.pageY > this.element.offsetHeight + this.offsetTop - this.zoneHeight;
 
-    console.log(evt.pageY, topZone, bottomZone);
-
     if (!this.intervalId && topZone && this.element.scrollTop > this.scrollSize) {
       const scrollBy = -1 * this.scrollSize;
-      console.log('scrollBy', scrollBy);
       this.intervalId = window.setInterval(() => { this.element.scrollBy(0, scrollBy); }, intervalTime);
     } else if (!this.intervalId && bottomZone && this.element.scrollHeight - this.element.scrollTop > this.element.offsetHeight) {
       const scrollBy = this.scrollSize;
-      console.log('scrollBy', scrollBy);
       this.intervalId = window.setInterval(() => { this.element.scrollBy(0, scrollBy); }, intervalTime);
     } else if (this.intervalId && (!topZone || this.element.scrollTop <= this.scrollSize)) {
-      console.log('clear');
       window.clearInterval(this.intervalId);
       this.intervalId = null;
     }
@@ -73,5 +57,4 @@ class ScrollZone {
       return offset;
     }
   };
-
 }
